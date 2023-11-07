c1e46fcb27edc43fc26ae5a79953c7370fd2c432
natchapon <natchaponrakcharoen@gmail.com>
2023-11-07 10:11:06 +0900

    Add files via upload

---
+++ go_advance/myIdflags/auth/protect.go
@@ -0,0 +1,33 @@
+package auth
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+func Protect(signature []byte) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		auth := ctx.Request.Header.Get("Authorization")
+		tokenString := strings.TrimPrefix(auth, "Bearer ")
+
+		_, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+			}
+
+			return signature, nil
+		})
+
+		if err != nil {
+			ctx.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		ctx.Next()
+	}
+
+}
