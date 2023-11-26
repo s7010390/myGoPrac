9bab2867871a2df31a726b8ca7fcab42e3b22a38
DESKTOP-8RAF2QM\ss13 <natchaponrakcharoen@gmail.com>
2023-11-26 11:47:55 +0900

    update

--- go_advance/liveness_probe/main.go
+++ go_advance/liveness_probe/main.go
@@ -1,13 +1,8 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"net/http"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	auth "github.com/s7010390/myGoPrac/go_advance/liveness_probe/auth"
@@ -63,31 +58,5 @@ func main() {
 	handler := todo.NewTodoHandler(db)
 	protected.POST("/todos", handler.NewTask)
 
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-	defer stop()
-
-	s := &http.Server{
-		Addr:           ":" + os.Getenv("PORT"),
-		Handler:        r,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
-
-	go func() {
-		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal("listen: %s\n", err)
-		}
-	}()
-
-	<-ctx.Done()
-	stop()
-	fmt.Println("Shutting down gracefully, press Ctrl+C again to force")
-
-	timeoutCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	if err := s.Shutdown(timeoutCtx); err != nil {
-		fmt.Println(err)
-	}
+	r.Run()
 }
